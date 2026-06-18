f4e27276582e942d46e1047aa24f9169415a9002
fqhd <fahd31256@gmail.com>
2026-06-18 16:54:45 +0100

    chore(cmake): add build system

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_MyQtApp_FILE /home/fahd/GitHub/EngineBoard/build/MyQtApp)
+set(__QT_DEPLOY_TARGET_MyQtApp_TYPE EXECUTABLE)
---
+++ build/CMakeFiles/MyQtApp.dir/MyQtApp_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/MyQtApp.dir/MyQtApp_autogen/mocs_compilation.cpp.o: \
+ /home/fahd/GitHub/EngineBoard/build/MyQtApp_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
